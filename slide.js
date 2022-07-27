b4622cd0d97c0895b86900838328635cb66c5bf5
Rafa2Fz <rafaelfrohlich@hotmail.com>
2022-07-27 16:53:38 -0300

    Adicionado a função e os botões para alterar o slide para o próximo ou anterior

--- slide.js
+++ slide.js
@@ -1,22 +1,61 @@
-let time = 5000
+let time = 4000
 let currentImageIndex = 0
+
 let images = document.querySelectorAll("#slider img")
 let max = images.length; // coloca todas as imagens em na váriavel max
+let previous = document.getElementById("previous")
+let next = document.getElementById("next")
 
-function nextImage(){
+function setSlide(newIndex) {
+    images[newIndex].classList.add("selected")
+}
+
+function removeSlide(currentImageIndex) {
     images[currentImageIndex].classList.remove("selected")
-
+}
+function nextImage() {
+    removeSlide(currentImageIndex)
     currentImageIndex++
-
-    if(currentImageIndex >= max){
+    if (currentImageIndex >= max) {
         currentImageIndex = 0
     }
-    images[currentImageIndex].classList.add("selected")
+    setSlide(currentImageIndex)
 }
 
 
-function start(){
-    setInterval(nextImage, time)
+function createInterval() {
+
+    let interval = setInterval(() => {
+        nextImage()
+    }, time);
+    return interval
+}
+function start() {
+    let interval = createInterval()
+
+    previous.addEventListener('click', event => {
+        event.stopPropagation();
+        clearInterval(interval)
+
+
+        removeSlide(currentImageIndex)
+        currentImageIndex--
+        if (currentImageIndex < 0) currentImageIndex = max - 1
+
+        setSlide(currentImageIndex)
+        interval = createInterval()
+
+    })
+
+    next.addEventListener('click', event => {
+        event.stopPropagation();
+        clearInterval(interval)
+        removeSlide(currentImageIndex)
+        currentImageIndex++
+        if (currentImageIndex >= max) currentImageIndex = 0
+        setSlide(currentImageIndex)
+        interval = createInterval()
+    })
 }
 
 // executa função quando a página for carregada
